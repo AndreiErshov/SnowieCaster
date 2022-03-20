7fe5357f8e434158baa4bb33e83dc6682f22bdf5
AndreiErshov <mtaprovince04@gmail.com>
2022-03-20 16:53:26 +0300

    added release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="snowiecaster",
-    version="0.0.4",
+    version="0.0.5",
     author="Andrei Ershov",
     author_email="author@example.com",
     description="An easy pub/sub python library",
